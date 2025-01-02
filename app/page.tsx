bd9333563d4629be075a6821e2b269e20eb0bda8
PabloReyes <andresreyes.ceo@gmail.com>
2025-01-01 19:54:43 -0500

    finalizando 2/10

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import CoverParticles from '@/components/cover-particles'
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
+        <CoverParticles/>
+        <p>introduction</p>
+      </div>
+    </main>
+  );
+}
